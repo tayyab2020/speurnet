022b7e84862a5b8611cc5628ad4958fb8f068469
Muhammad Tayyab <tayyabkhurram62@gmail.com>
2020-07-13 02:49:13 +0500

    updates

--- resources/views/pages/agentsingle.blade.php
+++ resources/views/pages/agentsingle.blade.php
@@ -1098,14 +1098,13 @@
 
             var agent_map_check = $('#agent_map_check').val();
 
-            if(agent_map_check == 1)
-            {
-                
                 $(window).load(function() {
-                    console.log('here');
-                    agent_initMap();
+                    if(agent_map_check == 1) {
+                        console.log('here');
+                        agent_initMap();
+                    }
                 });
-            }
+
 
 
             function agent_handleLocationError(browserHasGeolocation1, infoWindow1, type1) {
