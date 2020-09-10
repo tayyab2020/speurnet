cee8c06d38a875cd66ce31e0132b8dc927d1063c
Muhammad Tayyab <tayyabkhurram62@gmail.com>
2020-09-10 11:48:31 +0500

    email

--- config/mail.php
+++ config/mail.php
@@ -120,14 +120,6 @@ return [
         ],
     ],
 
-    'stream' => [
-        'ssl' => [
-            'verify_peer' => false,
-            'verify_peer_name' => false,
-            'allow_self_signed' => true,
-        ],
-    ],
-
     /*
     |--------------------------------------------------------------------------
     | Log Channel
