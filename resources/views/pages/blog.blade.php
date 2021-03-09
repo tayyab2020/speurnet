c13befc08a4ac4d6491afffc1d6e39d8ba7b2232
Muhammad Tayyab <tayyabkhurram62@gmail.com>
2021-03-09 14:17:28 +0500

    updates

--- resources/views/pages/blog.blade.php
+++ resources/views/pages/blog.blade.php
@@ -644,7 +644,7 @@
                 .bulgy-radios .label
                 {
                     font-size: 75%;
-                    padding: 10px 10px 0 0;
+                    padding: 9.5px 10px 0 0;
                 }
 
                 .radio
