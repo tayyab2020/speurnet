a7fe904251722847a8abaf9626da66a0488c162a
Muhammad Tayyab <tayyabkhurram62@gmail.com>
2021-05-28 02:10:03 +0500

    distance matrix update

--- app/Http/Controllers/PropertiesController.php
+++ app/Http/Controllers/PropertiesController.php
@@ -1341,29 +1341,27 @@ class PropertiesController extends Controller
                      $property_latitude = $key->map_latitude;
                      $property_longitude = $key->map_longitude;
 
-
-                     $url = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=".urlencode($address_latitude).",".urlencode($address_longitude)."&destinations=".urlencode($property_latitude).",".urlencode($property_longitude)."&key=AIzaSyChgcpFYG5aLIvd_U0Xmx6ry7jlL9NI_BU";
-
-                     $result_string = file_get_contents($url);
-                     $result = json_decode($result_string, true);
-
-
-                     if($result['rows'][0]['elements'][0]['status'] == 'OK')
+                     if($property_latitude && $property_longitude)
                      {
-                         $property_radius = $result['rows'][0]['elements'][0]['distance']['value'];
-                         $property_radius = $property_radius / 1000;
+                         $url = "https://dev.virtualearth.net/REST/v1/Routes/DistanceMatrix?origins=".urlencode($address_latitude).",".urlencode($address_longitude)."&destinations=".urlencode($property_latitude).",".urlencode($property_longitude)."&travelMode=driving&key=ApGfIF6Y_pCEfKLHWz7J4f60CkCs4XhRQW4DA95a_lI2ATGKnoZmF-aqCwANOQND";
 
-                         $property_radius = round($property_radius);
+                         $result_string = file_get_contents($url);
+                         $result = json_decode($result_string, true);
 
-                         if($property_radius <= $radius)
+                         if($result['statusCode'] == 200)
                          {
-                             array_push($properties_search,$key);
+                             $property_radius = $result['resourceSets'][0]['resources'][0]['results'][0]['travelDistance'];
+
+                             $property_radius = round($property_radius);
+
+                             if($property_radius <= $radius)
+                             {
+                                 array_push($properties_search,$key);
+                             }
                          }
                      }
 
-
                  }
-
 
                  $properties = $properties_search;
              }
