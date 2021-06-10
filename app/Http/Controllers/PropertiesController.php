68f5f658fca878e5a7250bcf5d1e8947afcd50be
Muhammad Tayyab <tayyabkhurram62@gmail.com>
2021-06-10 14:16:12 +0500

    updates

--- app/Http/Controllers/PropertiesController.php
+++ app/Http/Controllers/PropertiesController.php
@@ -1338,7 +1338,7 @@ class PropertiesController extends Controller
 
                  foreach ($properties->get() as $z => $key)
                  {
-                     sleep(1.5);
+                     /*sleep(1.5);*/
 
                      $property_latitude = $key->map_latitude;
                      $property_longitude = $key->map_longitude;
@@ -1361,7 +1361,7 @@ class PropertiesController extends Controller
                          }
                      }*/
 
-                     /*$theta = $address_longitude - $property_longitude;
+                     $theta = $address_longitude - $property_longitude;
                      $dist = sin(deg2rad($address_latitude)) * sin(deg2rad($property_latitude)) +  cos(deg2rad($address_latitude)) * cos(deg2rad($property_latitude)) * cos(deg2rad($theta));
                      $dist = acos($dist);
                      $dist = rad2deg($dist);
@@ -1381,31 +1381,14 @@ class PropertiesController extends Controller
                      if($property_radius <= $radius)
                      {
                          array_push($properties_search,$key);
-                     }*/
+                     }
 
-                     if($property_latitude && $property_longitude)
+                     /*if($property_latitude && $property_longitude)
                      {
-                         $ch = curl_init();
-                         $headers = array(
-                             'Accept: application/json',
-                             'Content-Type: application/json',
+                         $url = "https://dev.virtualearth.net/REST/v1/Routes/DistanceMatrix?origins=".urlencode($address_latitude).",".urlencode($address_longitude)."&destinations=".urlencode($property_latitude).",".urlencode($property_longitude)."&travelMode=driving&key=ApGfIF6Y_pCEfKLHWz7J4f60CkCs4XhRQW4DA95a_lI2ATGKnoZmF-aqCwANOQND";
 
-                         );
-                         curl_setopt($ch, CURLOPT_URL, "https://dev.virtualearth.net/REST/v1/Routes/DistanceMatrix?origins=".urlencode($address_latitude).",".urlencode($address_longitude)."&destinations=".urlencode($property_latitude).",".urlencode($property_longitude)."&travelMode=driving&key=ApGfIF6Y_pCEfKLHWz7J4f60CkCs4XhRQW4DA95a_lI2ATGKnoZmF-aqCwANOQND");
-                         curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-                         curl_setopt($ch, CURLOPT_HEADER, 0);
-
-                         curl_setopt( $ch, CURLOPT_CUSTOMREQUEST, 'GET' );
-                         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-                         // Timeout in seconds
-                         curl_setopt($ch, CURLOPT_TIMEOUT, 30);
-
-                         $response = curl_exec($ch);
-
-                         curl_close($ch);
-
-                         $result = json_decode($response, true);
+                         $result_string = file_get_contents($url);
+                         $result = json_decode($result_string, true);
 
                          if($result['statusCode'] == 200)
                          {
@@ -1418,7 +1401,7 @@ class PropertiesController extends Controller
                                  array_push($properties_search,$key);
                              }
                          }
-                     }
+                     }*/
 
                  }
 
