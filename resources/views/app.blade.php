a95e64a2a98b86d9379f639521aa613203d52c0f
Muhammad Tayyab <tayyabkhurram62@gmail.com>
2022-01-12 13:50:35 +0500

    Updates

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -56,6 +56,18 @@
 
 	{!! getcong('addthis_share_code')!!}
 
+    <!-- Smartsupp Live Chat script -->
+    <script type="text/javascript">
+        var _smartsupp = _smartsupp || {};
+        _smartsupp.key = 'c48884354ee46ae0e7e98458fad7375341bdcfcf';
+        window.smartsupp||(function(d) {
+            var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+            s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+            c.type='text/javascript';c.charset='utf-8';c.async=true;
+            c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+        })(document);
+    </script>
+
 
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-ZFVPPBHJJX"></script>
